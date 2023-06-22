af05e6360959cd14b490a4f27549f9b59787e16b
Daniel Ilievski <ilievskiot@hotmail.com>
2023-06-22 07:54:33 +0200

    Update the security group configuration

--- ec2-nginx.tf
+++ ec2-nginx.tf
@@ -1,6 +1,6 @@
 # Configure the AWS provider
 provider "aws" {
-  region = "eu-west-1"
+  region = var.aws_region
 }
 
 # Data source to retrieve the latest Ubuntu 20.04 LTS AMI
@@ -64,35 +64,27 @@ resource "aws_instance" "web_server" {
 
 # Create a security group allowing inbound HTTP traffic
 resource "aws_security_group" "web_server_sg" {
-  name        = "web-server-sg"
-  description = "Allow HTTP inbound traffic"
+  name        = var.web_server_sg_name
+  description = var.web_server_sg_description
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value["from_port"]
+      to_port     = ingress.value["to_port"]
+      protocol    = ingress.value["protocol"]
+      cidr_blocks = ingress.value["cidr_blocks"]
+    }
   }
 
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      from_port   = egress.value["from_port"]
+      to_port     = egress.value["to_port"]
+      protocol    = egress.value["protocol"]
+      cidr_blocks = egress.value["cidr_blocks"]
+    }
   }
 }
 
